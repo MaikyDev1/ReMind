9f7adb1e7fc0f1f606bec50a52561b6c5283e339
MaikyDev <maikydev@icloud.com>
2023-05-09 16:14:34 +0300

    first commit

--- src/routes/authorization_route.js
+++ src/routes/authorization_route.js
@@ -4,7 +4,11 @@ const bcrypt = require('bcrypt');
 // REGISTER ROUTE
 
 router.post('/authorization/register', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password);
+    } catch {
 
+    }
 })
 
 router.get('/authorization/register', async (req, res) => {
